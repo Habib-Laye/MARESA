6fbd4fd1f1c02162db33df768d9740d826090bf4
Habib-Laye <71091192+Habib-Laye@users.noreply.github.com>
2021-08-10 08:54:15 +0200

    Version 2, Ajout de salon.php pour la recherche des salons de coiffure

---
+++ dossier_php/salon.php
@@ -0,0 +1,35 @@
+
+<?php
+    require 'header.php'
+?>
+
+</br>  
+    <h1><p class='col-lg-8 offset-lg-5'>Salon de coiffure</p></h1>
+
+</br>
+
+<div class="formulaire">
+
+    <form method="POST" action="salontraitement.php" class='col-lg-8 offset-lg-5'>
+        <div class="form-group">
+            <input type="text" placeholder= "Nom" name="nom">
+        </div>
+
+        <div class="form-group">
+            <input type="text" placeholder= "Ville" name="ville">
+        </div>
+
+        <div class="form-group">
+            <input type="text" placeholder= "Code postal" name="code_postal">
+        </div>
+
+        <div class="form-group">
+            <input type="text" placeholder= "Adresse" name="adresse">
+        </div>
+
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary" name="rechercher">Rechercher</button>
+        </div>
+    </form>
+
+</div>
