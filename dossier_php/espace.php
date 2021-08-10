40ae8e71911b35222e09488a8fae1c6aa626c393
Habib-Laye <71091192+Habib-Laye@users.noreply.github.com>
2021-08-10 08:44:58 +0200

    Version 2, Ajout de espace.php

---
+++ dossier_php/espace.php
@@ -0,0 +1,71 @@
+
+        
+        <?php
+        require 'header.php'
+        ?>
+        </br>
+        <h1><p class='col-lg-8 offset-lg-4'>Gérer vos informations</p></h1>
+        </br>
+        
+                <div class="container-fluid">
+                    <div class="row">
+                      <div class="col">
+                        <p>Mettre une photo de votre établissement</p>
+                        <img src="image/photo1.PNG"><br>
+                        <button id="bout" type="submit">Modifier</button>
+                        <button id="bout" type="submit">supprimer</button>
+
+                      </div>
+                      <div class="col">
+                        <p>Mettre la plage des horaires d'ouvertures</p>
+                        <img src="image/photo2.PNG">
+                      </div>
+
+                    </div>
+                    
+                    </br>
+                    <div class="row">
+                      <div class="col-4">
+                        Service proposé 1 <br>
+                        <img src="image/photo3.PNG"><br>
+                        <button type="submit">Modifier</button>
+                        <button type="submit">supprimer</button>
+
+                      </div>
+                      <div class="col-4">
+                        Service proposé 2 <br>
+                        <img src="image/photo4.PNG"><br>
+                        <button type="submit">Modifier</button>
+                        <button type="submit">supprimer</button>
+
+                      </div>
+                      <div class="col-4">
+                        Service proposé 3 <br>
+                        <img src="image/photo5.PNG"><br>
+                        <button type="submit">Modifier</button>
+                        <button type="submit">supprimer</button>
+
+                      </div>
+                    </div>
+                  </div>
+                </br>
+
+    <!-- footer -->
+
+    <div class="card text-center">
+        <div class="card-header">
+          Maresa
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">En savoir plus</h5>
+          <p class="card-text">Maresa vous remercie de l'importance que vous lui apportez.Vous 
+              avez choisi la meilleur solution. Prenez vos rdv-vous en un clic</p>
+          <a href="#" class="btn btn-primary">Consulter</a>
+        </div>
+        <div class="card-footer text-muted">
+          Il y a deux jours
+        </div>
+      </div>
+
+</body>
+</html>
