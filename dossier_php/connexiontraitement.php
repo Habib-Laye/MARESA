065526eb2bb5c3c67905fbca3c4dc3c486ed2b6b
Habib-Laye <71091192+Habib-Laye@users.noreply.github.com>
2021-08-11 19:26:42 +0200

    Ajout de connexiontraitement.php, c'est le fichier qui permet de traiter les informations venant du formulaire de connexion: connexion.php

---
+++ dossier_php/connexiontraitement.php
@@ -0,0 +1,41 @@
+
+<?php
+
+    require_once'db.php';
+    //instance de la base de données
+    //$bdd = new PDO('mysql:host=127.0.0.1;dbname=maresa_bdd', 'root', '');
+    //$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    //Récupérer l'email rempli par l'utilisateur
+
+    $email = $_POST["email"];
+    //  Récupération de l'utilisateur et de son mot de passe hashé
+
+    $req = $bdd->prepare('SELECT id_compte, motdepasse FROM compte WHERE email = :email');
+    $req->execute(array(
+        'email' => $email));
+    $resultat = $req->fetch();
+    //le var_dump ci_dessous permet d'afficher toutes les infos sur la variable $resultat
+    //var_dump($resultat);
+    // Comparaison du mot de passe envoyé via le formulaire avec la base
+    $isPasswordCorrect = password_verify($_POST['motdepasse'], $resultat['motdepasse']);
+
+    if (!$resultat)
+    {
+         echo 'Mauvais identifiant !';
+    } else {
+        if ($isPasswordCorrect) {
+            session_start();
+            $_SESSION['id'] = $resultat['id_compte'];
+                
+            header('Location: espace.php');
+            //echo 'Vous êtes connecté ! ' . $_SESSION['nom'];
+            }
+            else {
+                echo 'Mauvais mot de passe !';
+            }
+        }
+
+
+
+?>
